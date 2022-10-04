5df403f2885a97ca9e71dc5f198100b3a0372080
unpopularcoder <unpopularcoder@proton.me>
2022-10-04 22:10:52 +0000

    ARM: 8037/1: mm: support big-endian page tables

--- utils.py
+++ utils.py
@@ -8,4 +8,4 @@ from texttable import Texttable
 from scipy import stats
 
 
-def computing_precision_ks(trues, predictions, ks, inclusive=
+def computing_precision_ks(trues, predictions, ks, inclusive=True, rm=0
