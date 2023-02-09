da91447db395f4b08a7167c214605ee09c6a0ce1
unpopularcoder <unpopularcoder@proton.me>
2023-02-09 05:36:02 +0000

    fixed merge problems

--- utils.py
+++ utils.py
@@ -30,4 +30,5 @@ def computing_precision_ks(trues, predictions, ks, inclusive=True, rm=0):
 
 def metrics_spearmanr_rho(true, predication):
     assert true.shape == predication.shape
-    rho, p_val = stats.spearmanr(true, pr
+    rho, p_val = stats.spearmanr(true, predication)
+ 
