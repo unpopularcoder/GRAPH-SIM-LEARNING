e421a8799a302c9c7f1f08696290f5e009c2db86
unpopularcoder <unpopularcoder@proton.me>
2023-02-09 22:38:30 +0000

    typo fixed

--- utils.py
+++ utils.py
@@ -31,4 +31,6 @@ def computing_precision_ks(trues, predictions, ks, inclusive=True, rm=0):
 def metrics_spearmanr_rho(true, predication):
     assert true.shape == predication.shape
     rho, p_val = stats.spearmanr(true, predication)
-    ret
+    return rho
+
+
