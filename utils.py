41f85197e480d226baea1efdd8b1af7f799cafe2
unpopularcoder <unpopularcoder@proton.me>
2023-03-01 12:05:38 +0000

    regular commit

--- utils.py
+++ utils.py
@@ -36,4 +36,5 @@ def metrics_spearmanr_rho(true, predication):
 
 def metrics_kendall_tau(true, predication):
     assert true.shape == predication.shape
-    tau, p_val = stats.kendalltau(true, predicati
+    tau, p_val = stats.kendalltau(true, predication)
+   
