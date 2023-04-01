270097ed4af8b9c8e385dcac4fc5b409a6f6dd68
unpopularcoder <unpopularcoder@proton.me>
2023-04-01 06:37:06 +0000

    docs(changelog): build

--- utils.py
+++ utils.py
@@ -48,4 +48,5 @@ def metrics_mean_square_error(true, predication):
 
 def tab_printer(args):
     """
-    Function to print the logs in a nice tabular forma
+    Function to print the logs in a nice tabular format.
+  
