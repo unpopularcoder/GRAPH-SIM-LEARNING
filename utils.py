61ce5cdaad04e16cf421db2e9689df1610e316ae
unpopularcoder <unpopularcoder@proton.me>
2023-04-23 08:34:28 +0000

    * [RELEASE]prepare for next development iteration

--- utils.py
+++ utils.py
@@ -55,4 +55,5 @@ def tab_printer(args):
     keys = sorted(args.keys())
     t = Texttable()
     t.set_cols_dtype(['t', 't'])
-    t.add_rows([['Parameter', 'Value']] + [[k.replace('_', ' ').capitalize(), args[k]] for k in keys]
+    t.add_rows([['Parameter', 'Value']] + [[k.replace('_', ' ').capitalize(), args[k]] for k in keys])
+    prin
