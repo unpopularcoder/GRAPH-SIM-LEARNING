59b570bdd057cd13ef6b75a5966a07a80f9e5d5e
unpopularcoder <unpopularcoder@proton.me>
2023-05-29 08:01:52 +0000

    fix: Typo

--- utils.py
+++ utils.py
@@ -65,4 +65,5 @@ def top_k_ids(data, k, inclusive, rm):
     :param k:
     :param inclusive: whether to be tie inclusive or not.
         For example, the ranking may look like this:
-        7 (sim_score=0.99), 5 (sim_score=0.99), 10 (sim_score=0.98)
+        7 (sim_score=0.99), 5 (sim_score=0.99), 10 (sim_score=0.98), ...
+    
