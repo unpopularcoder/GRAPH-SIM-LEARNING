ee9d0338b8e3c1301d52bb3a7deac463202c0e8d
unpopularcoder <unpopularcoder@proton.me>
2023-07-12 12:57:02 +0000

    Preparing release

--- utils.py
+++ utils.py
@@ -78,4 +78,4 @@ def top_k_ids(data, k, inclusive, rm):
     sort_id_mat = np.argsort(-data)
     n = sort_id_mat.shape[0]
     if k < 0 or k >= n:
-        raise RuntimeError('Invalid k {}'.fo
+        raise RuntimeError('Invalid k {}'.format(k))
