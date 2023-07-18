240feaaa78f53fb7dd16bc7bac2097c0af024719
unpopularcoder <unpopularcoder@proton.me>
2023-07-18 21:50:26 +0000

    Bugfixing.

--- utils.py
+++ utils.py
@@ -80,4 +80,5 @@ def top_k_ids(data, k, inclusive, rm):
     if k < 0 or k >= n:
         raise RuntimeError('Invalid k {}'.format(k))
     if not inclusive:
-        return sort_id_ma
+        return sort_id_mat[:k]
+    # 
