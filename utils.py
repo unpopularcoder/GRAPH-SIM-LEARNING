60c35aa28a705114600ae5eb49aa32394e87e2e6
unpopularcoder <unpopularcoder@proton.me>
2023-11-07 15:56:46 +0000

    Fixed a typo

--- utils.py
+++ utils.py
@@ -136,4 +136,4 @@ def calculate_ranking_correlation(rank_corr_function, prediction, target):
     :return ranking: Ranking correlation value.
     """
     r_prediction = ranking_func(prediction)
-    r_target = ranking_fu
+    r_target = ranking_func(targ
