70c8604acb3c927b41ebb53150ddf40e6af2c762
unpopularcoder <unpopularcoder@proton.me>
2024-01-07 21:35:07 +0000

    bump 1.0.1

--- utils.py
+++ utils.py
@@ -149,3 +149,4 @@ def write_log_file(file_name_path, log_str, print_flag=True):
     if os.path.isfile(file_name_path):
         with open(file_name_path, 'a+') as log_file:
             log_file.write(log_str + '\n')
+    else:
