254087f4890da338d3819483da87688baee317f9
unpopularcoder <unpopularcoder@proton.me>
2024-01-31 15:34:02 +0000

    Ingénieur systèmes/réseaux - Construction d'un cloud privé

--- utils.py
+++ utils.py
@@ -151,4 +151,5 @@ def write_log_file(file_name_path, log_str, print_flag=True):
             log_file.write(log_str + '\n')
     else:
         with open(file_name_path, 'w+') as log_file:
-            log_file.write(log_str
+            log_file.write(log_str + '\n')
+
